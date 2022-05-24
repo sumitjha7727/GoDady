10b1540cb19105935d59a26eddc02fce9f3dd0f1
Sumit Kant Jha <79800728+sumitjha7727@users.noreply.github.com>
2022-05-24 22:51:32 +0530

    Add files via upload

---
+++ GoDady/MaximizeBrowser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoQASite;
+using NUnit.Framework;
+
+namespace GoDaddy
+{
+    class MaximizeBrowser : Selenium_Base
+    {
+       public void MaxAndClose()
+        {
+            open("https://www.godaddy.com/");
+            wait(2000);
+            close();
+        }
+    }
+}
---
+++ GoDady/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DemoQASite;
+using NUnit.Framework;
+
+namespace GoDaddy
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            MaximizeBrowser maximizeBrowser = new MaximizeBrowser();
+            maximizeBrowser.MaxAndClose();
+        }
+    }
+}
